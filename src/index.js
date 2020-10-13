585ddf0b2ebce468de8bf40432e6fc30ebee7bc4
nancy <337232241@qq.com>
2020-10-13 16:03:27 +0800

    feat:createelement

--- src/index.js
+++ src/index.js
@@ -1,4 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-let element = <h1>this is Element</h1>
-ReactDOM.render(element, document.body);
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+import React from './myreact';
+let ReactDOM = React;
+
+let element = <div>
+    <h1>this is subTitle</h1>
+    <p>this is content</p>
+    <div>
+        <h1>this is subTitle</h1>
+        <p>this is sub content</p>
+    </div>
+</div>;
+ReactDOM.render(element, document.getElementById('root'));
---
+++ src/myreact/index.js
@@ -0,0 +1,17 @@
+const createElement = (type, props, ...children)=> {
+    delete props.__source;
+    return {
+        type,
+        props: {
+            ...props,
+            children,
+        }
+    }
+};
+const render = (vdom, container)=> {
+    container.innerHTML = `<pre>${JSON.stringify(vdom, null, 2)}</pre>`
+};
+export default {
+    createElement,
+    render
+}
